04730c2620c56a2f73fa74aca7d446abca0ba2d3
Soporte <Soporte@Q-ITSolitions>
2022-07-31 22:40:35 -0500

    actualiacion de campos

--- src/java/com/gruposw/TblUsuario.java
+++ src/java/com/gruposw/TblUsuario.java
@@ -12,6 +12,8 @@ import javax.persistence.Basic;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
@@ -46,7 +48,7 @@ public class TblUsuario implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
     @Basic(optional = false)
-    @NotNull
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "ID_Usuario")
     private Integer iDUsuario;
     @Size(max = 50)
